13adf646b45fcf9291435a26dfbe3b8fb7599187
gustavs121 <gsalmor@gmail.com>
2026-09-23 18:47:54 -0400

    Create simple-interest.sh

---
+++ simple-interest.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+# This script calculates simple interest given principal,
+# annual rate of interest and time period in years.
+
+# Do not use this in production. Sample purpose only.
+
+# Author: Git and GitHub Expert
+# Formula: Simple Interest = (principal * rate * time) / 100
+
+echo "Enter the principal:"
+read principal
+
+echo "Enter rate of interest per year (%):"
+read rate
+
+echo "Enter time period in years:"
+read time
+
+# Calculate simple interest using bc for floating-point arithmetic support
+interest=\((echo "scale=2; (\)principal * \(rate *\)time) / 100" | bc)
+
+echo "The simple interest is: $interest"
