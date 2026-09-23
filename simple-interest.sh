fc796b10fdf3eedde6cc8ad0856ea7b3f4139e48
gustavs121 <gsalmor@gmail.com>
2026-09-23 19:21:44 -0400

    Update simple-interest.sh

--- simple-interest.sh
+++ simple-interest.sh
@@ -22,3 +22,12 @@ interest=\((echo "scale=2; (\)principal * \(rate *\)time) / 100" | bc)
 echo "The simple interest is: $interest"
 git checkout main
 git merge bug-fix-typo
+git checkout main
+Switched to branch 'main'
+Your branch is up to date with 'origin/main'.
+git merge bug-fix-typo
+Updating a0b1c2d..e3f4g5h
+Fast-forward
+ README.md | 2 +-
+ 1 file changed, 1 insertion(+), 1 deletion(-)
+ 
